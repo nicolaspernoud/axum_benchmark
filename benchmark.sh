e48f0bea94447f90decf4b862de465effb0bd11b
Nicolas Pernoud <github@ninico.fr>
2023-04-17 09:49:15 +0200

    fix: function for testing axum versions

--- benchmark.sh
+++ benchmark.sh
@@ -7,17 +7,40 @@ WD="$(
 )"
 
 mkdir $WD/reports
-REPORT_FILE="$WD/reports/$(date +"%Y-%m-%d_%H:%M:%S")_test.txt"
+REPORT_FILE="$WD/reports/$(date +"%Y-%m-%d_%H:%M:%S")_merge.txt"
 
 # Not a proxy anymore, is a dummy for this test
 PROXY=http://app1.atrium.127.0.0.1.nip.io:8080
 BENCH_CMD="rewrk -c 400 -t 8 -d 20s -h ${PROXY} --pct >> $REPORT_FILE"
 
-test_proxy() {
+axum_bench() {
+  # Build for production
+  cd ${WD}
+  # Switch to target version
+  sed -i "s/axum = { version=\"=[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\"/axum = { version=\"=$1\"/g" Cargo.toml
+  # Test that switch was made properly
+  if grep -q $1 Cargo.toml; then
+    echo "Changed Axum version in Cargo.toml successfully !"
+  else
+    echo "Error: Changing Axum version in Cargo.toml failed !"
+    exit 1
+  fi
+  cargo build --release
+  # Start proxy
+  cp ${WD}/atrium.yaml ${WD}/target/release/
+  cd ${WD}/target/release/
+  ./atrium &
+  TEST_PROXY_PID=$!
+  sleep 2
+  # Test proxy
+  echo -e "####################\n### AXUM $1  ###\n####################\n" >>$REPORT_FILE
   if [ "$(curl -s ${PROXY})" != "Hello World!" ]; then
     echo "Error: curl command did not return 'Hello World!'"
     exit 1
   fi
+  eval ${BENCH_CMD}
+  # Shutdown
+  kill $TEST_PROXY_PID
 }
 
 #####################################################################
@@ -28,49 +51,11 @@ sudo apt install -y libssl-dev
 sudo apt install -y pkg-config
 cargo install rewrk --git https://github.com/ChillFish8/rewrk.git
 
-#####################################################################
-#                             AXUM 0.6.12                           #
-#####################################################################
+####################################################################
+#                            DO THE TESTS                          #
+####################################################################
 
-# Build for production
-cd ${WD}
-# Switch back to 0.6.12
-sed -i 's/axum = { version="=0.6.15"/axum = { version="=0.6.12"/g' Cargo.toml
-cat Cargo.toml | grep "axum = { version"
-cargo build --release
-# Start proxy
-cp ${WD}/atrium.yaml ${WD}/target/release/
-cd ${WD}/target/release/
-./atrium &
-ATRIUM_PROXY_PID=$!
-sleep 2
-# Test proxy
-echo -e "####################\n### AXUM 0.6.12  ###\n####################\n" >>$REPORT_FILE
-test_proxy
-eval ${BENCH_CMD}
-# Shutdown
-kill $ATRIUM_PROXY_PID
-
-#####################################################################
-#                             AXUM 0.6.15                           #
-#####################################################################
-
-# Build for production
-cd ${WD}
-# Switch to axum 0.6.15
-sed -i 's/axum = { version="=0.6.12"/axum = { version="=0.6.15"/g' Cargo.toml
-cat Cargo.toml | grep "axum = { version"
-cargo build --release
-# Start proxy
-cd ${WD}/target/release/
-./atrium &
-ATRIUM_PROXY_PID=$!
-sleep 2
-# Test proxy
-echo -e "####################\n### AXUM 0.6.15  ###\n####################\n" >>$REPORT_FILE
-test_proxy
-eval ${BENCH_CMD}
-# Shutdown
-kill $ATRIUM_PROXY_PID
-
-cat $REPORT_FILE
+axum_bench 0.6.12
+axum_bench 0.6.13
+axum_bench 0.6.14
+axum_bench 0.6.15
